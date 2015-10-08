e2b2064014988726e0b1c46fd5311665c4971bd3
biangang.bg <biangang.bg@alibaba-inc.com>
2015-10-08 19:09:23 +0800

    up

---
+++ webpack.config.js
@@ -0,0 +1,31 @@
+var ExtractTextPlugin = require('extract-text-webpack-plugin');
+var DEVMODE = process.env.NODE_ENV !== 'production';
+
+var styleLoader = 'css?sourceMap!autoprefixer?{browsers: ["last 2 versions", "not ie < 8"]}!less?sourceMap';
+var plugins = [];
+if (DEVMODE) {
+	plugins.push(
+		new ExtractTextPlugin('kuma.css')
+	);
+}
+
+module.exports = {
+	entry: './index',
+	output: {
+		filename: 'main.js',
+		publicPath: '/demo/'
+	},
+	module: {
+		loaders: [
+			{
+				test: /\.less$/,
+				loader: DEVMODE ? 'style!' + styleLoader:  ExtractTextPlugin.extract(styleLoader)
+			}, {
+	            test: /\.svg(\?v=\d+\.\d+\.\d+)?$/,
+	            loader: 'url?limit=10000&minetype=image/svg+xml'
+	        }
+		]
+	},
+	plugins: plugins,
+	devtool: 'source-map'
+};
